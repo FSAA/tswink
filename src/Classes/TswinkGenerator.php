f3add21e4581b80813a7b0f974045b066c8e7419
Jonathan Pelchat <jonathan.pelchat@fsaa.ulaval.ca>
2019-09-04 11:36:25 -0400

    If the models support it, the plugin will now generate their relations, appends and hidden too. Add options to:  - Make all properties nullable (except for type any)  - Make properties optional when column is nullable in DB  - Change tabs into space for generated files (also can specify the number of spaces)  - Path to the models (will be used to generate the model relations if they are compatible)

--- src/Classes/TswinkGenerator.php
+++ src/Classes/TswinkGenerator.php
@@ -8,6 +8,9 @@ use Doctrine\DBAL\Schema\Column;
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Types\IntegerType;
 use Doctrine\DBAL\Types\StringType;
+use File;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 Class TswinkGenerator extends Generator
 {
@@ -20,6 +23,18 @@ Class TswinkGenerator extends Generator
     /** @var string */
     private $destination;
 
+    /** @var bool */
+    private $allPropertiesNullable;
+
+    /** @var bool */
+    private $testForOptionalProperties;
+
+    /** @var array */
+    private $classesByTable;
+
+    /** @var string */
+    private $indentation;
+
     public function __construct()
     {
         parent::__construct();
@@ -28,6 +43,10 @@ Class TswinkGenerator extends Generator
 
         if(function_exists('config')) {
             $this->destination = base_path(config('tswink.ts_classes_destination'));
+            $this->allPropertiesNullable = config('tswink.ts_all_properties_nullable');
+            $this->testForOptionalProperties = config('tswink.ts_test_for_optional_properties');
+            $this->classesByTable = $this->getModels();
+            $this->indentation = config('tswink.ts_spaces_instead_of_tabs') ? str_repeat(' ', config('tswink.ts_indentation_number_of_spaces')) : "\t";
         }
     }
 
@@ -47,17 +66,68 @@ Class TswinkGenerator extends Generator
 
     private function fileName()
     {
-        return str_singular(kebab_case(camel_case($this->table->getName())));
+        return $this->getTableFileName($this->table->getName());
+    }
+
+    private function getTableFileName($tableName)
+    {
+        return str_singular(kebab_case(camel_case($tableName)));
     }
 
     private function getClassContent()
     {
+        // Get the Model for the selected table
+        $model = null;
+        $hidden = [];
+        if (isset($this->classesByTable[$this->table->getName()])) {
+            $model = new $this->classesByTable[$this->table->getName()];
+            // Load the hidden props
+            $hidden = $model->getHidden();
+        }
+
+        // Generate the interface
         $tsClass = "export default interface {$this->getTableNameForClassFile()} {\n";
         foreach ($this->table->getColumns() as $column) {
-            $name = TswinkGenerator::escapeName($column->getName());
-            $tsClass .= "\t{$name}: {$this->getSimplifiedType($column)};\n";
+            if (!$hidden || !in_array($column->getName(), $hidden)) {
+                $name = TswinkGenerator::escapeName($column->getName());
+                $type = $this->getSimplifiedType($column);
+                if ($this->allPropertiesNullable && $type !== 'any') {
+                    $type .= ' | null';
+                }
+                $optionalProperty = (!$this->testForOptionalProperties || $column->getNotnull()) ? '' : '?';
+                $tsClass .= $this->indentation . "{$name}{$optionalProperty}: {$type};\n";
+            }
         }
-        return $tsClass . "}\n";
+
+        // Take care of the relations
+        $imports = '';
+        $imported = [];
+        if ($model && count($model->getModelRelations()) > 0) {
+            $tsClass .= "\n\n";
+            foreach ($model->getModelRelations() as $relation) {
+                if (!$hidden || !in_array($relation['relationName'], $hidden)) {
+                    $targetModelName = substr($relation['targetClass'], strrpos($relation['targetClass'], '\\') + 1);
+                    if ($relation['targetClass'] !== $this->classesByTable[$this->table->getName()]) {
+                        $targetFileName = $this->getTableFileName(array_search($relation['targetClass'], $this->classesByTable));
+                        if (!in_array($targetModelName, $imported)) {
+                            $imported[] = $targetModelName;
+                            $imports .= "import {$targetModelName} from './{$targetFileName}';\n";
+                        }
+                    }
+                    $isCollection = $relation['relationType'] === HasMany::class || $relation['relationType'] === BelongsToMany::class;
+                    if ($isCollection) {
+                        $tsClass .= $this->indentation . "{$relation['relationName']}?: {$targetModelName}[] | { [key: string]: $targetModelName };\n";
+                    } else {
+                        $tsClass .= $this->indentation . "{$relation['relationName']}?: {$targetModelName} | null;\n";
+                    }
+                }
+            }
+
+            foreach ($model->getAppends() as $append) {
+                $tsClass .= $this->indentation . "{$append}?: any;\n";
+            }
+        }
+        return $imports . "\n" . $tsClass . "}\n";
     }
 
     private static function escapeName($name){
@@ -104,5 +174,43 @@ Class TswinkGenerator extends Generator
     public function setDestination($destination)
     {
         $this->destination = $destination;
+    }
+
+    public function getModels()
+    {
+        $modelsClasses = [];
+        $modelsPaths = config('tswink.models_paths');
+        if (is_array($modelsPaths) && count($modelsPaths) > 0) {
+            foreach ($modelsPaths as $modelsPath) {
+                $files = File::files(base_path($modelsPath));
+                foreach ($files as $file) {
+                    if ($file->getType() === 'file' && strtolower($file->getExtension()) === 'php') {
+                        $handle = fopen($file->getPathname(), 'r');
+                        $namespace = null;
+                        $className = null;
+                        // Find the namespace and the class of every file in the folder
+                        while (($namespace === null || $className === null) && ($buffer = fgets($handle)) !== false) {
+                            preg_match('/namespace\\n* *(.+)\\n*;/', $buffer, $namespaceMatches);
+                            if (count($namespaceMatches) > 0) {
+                                $namespace = $namespaceMatches[1];
+                            }
+                            preg_match('/class\\n* *([a-zA-Z]+[a-zA-Z0-9]*)/', $buffer, $classMatches);
+                            if (count($classMatches) > 0) {
+                                $className = $classMatches[1];
+                            }
+                        }
+                        fclose($handle);
+
+                        if ($namespace && $className) {
+                            $fullClassName = $namespace . '\\' . $className;
+                            $model = new $fullClassName;
+                            $modelsClasses[$model->getTable()] = $fullClassName;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $modelsClasses;
     }
 }
--- src/Config/tswink.php
+++ src/Config/tswink.php
@@ -1,6 +1,16 @@
 <?php
 
 return [
-    // Destination of typescript classes
+    // Destination of typescript interfaces
     'ts_classes_destination' => 'resources/assets/src/models',
+    // Set to true to make all the interfaces' properties nullable (if not of type any)
+    'ts_all_properties_nullable' => false,
+    // Set to true to make all the nullable column also nullable in the interfaces.
+    'ts_test_for_optional_properties' => false,
+    'ts_spaces_instead_of_tabs' => false,
+    // If using spaces instead of tabs, how many spaces?
+    'ts_indentation_number_of_spaces' => 4,
+
+    // Array of paths to the models (so you could include vendors models too). Paths need to be relative to the laravel install root folder
+    'models_paths' => []
 ];
